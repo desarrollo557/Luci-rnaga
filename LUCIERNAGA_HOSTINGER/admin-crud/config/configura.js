6609e1a536952f8531951cc8b8dd2b46b981fb5e
desarrollo557 <desarrollo@siar.com.co>
2026-08-13 12:55:01 -0500

    chore: remove legacy LUCIERNAGA_HOSTINGER app and untrack env secrets

--- LUCIERNAGA_HOSTINGER/admin-crud/config/configura.js
+++
@@ -1,5 +0,0 @@
-export const{
-    PORT = 3000,
-    SALT_ROUND = 10,
-    SECRET_JWT_KEY ='#$%&////ÑLJMJec_:{ñ{}ññ{ñ}ñ{ñ4567_71/*-+14faDSA34Ñ.,!"#$%&/(YHGE)}HGFN,.{{}**SCXZC3451+}}hgxca{}{-.1||°'
-} = process.env
